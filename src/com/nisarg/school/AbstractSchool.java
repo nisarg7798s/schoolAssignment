3f31b12c6f171d512b12fd83f6f8e3024d4c0537
Nisarg Shah <nisargshah@Nisargs-MacBook-Pro.local>
2021-09-09 12:37:51 -0400

    AbstractSchool added

---
+++ src/com/nisarg/school/AbstractSchool.java
@@ -0,0 +1,8 @@
+package com.nisarg.school;
+
+public abstract class AbstractSchool implements School{
+    @Override
+    public Student admitStudent(String name, int age, String grade) {
+        return null;
+    }
+}
